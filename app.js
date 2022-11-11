c3b482a0203aabebea42fe7dfccf8a2e87b2a165
Dimitriy07 <babich.dimitriy@gmail.com>
2022-11-11 16:54:59 +0000

    Level Security 2-4

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
-console.log(process.env.API_KEY, process.env.SECRET);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -21,8 +22,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,17 +38,21 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login", function(req, res){
@@ -60,9 +63,11 @@ const password = req.body.password;
       console.log(err);
     } else {
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
